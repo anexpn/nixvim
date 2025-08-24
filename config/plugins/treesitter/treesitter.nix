320ce0949e672dc594c05db790d82da7e305f948
anexpn <875241499@qq.com>
2025-08-24 09:23:20 +0800

    Add treesitter functions

--- config/plugins/treesitter/treesitter.nix
+++ config/plugins/treesitter/treesitter.nix
@@ -1,7 +1,23 @@
 {
   plugins.treesitter = {
     enable = true;
+    settings = {
+      highlight = {
+        enable = true;
+      };
+      incremental_selection = {
+        enable = true;
+        keymaps = {
+          init_selection = "gnn";
+          node_decremental = "<A-i>";
+          node_incremental = "<A-o>";
+          scope_incremental = "<A-n>";
+        };
+      };
+      indent = {
+        enable = true;
+      };
+    };
     folding = true;
-    nixvimInjections = true;
   };
 }
