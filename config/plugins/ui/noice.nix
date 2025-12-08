0b41c010790892eae6486eb9dff61b0e278fcdaf
Jun <875241499@qq.com>
2025-12-09 03:55:27 +0800

    Noice checkhealth

--- config/plugins/ui/noice.nix
+++ config/plugins/ui/noice.nix
@@ -5,6 +5,12 @@
       notity = {
         enabled = false;
       };
+      lsp = {
+        override = {
+          "vim.lsp.util.convert_input_to_markdown_lines" = true;
+          "vim.lsp.util.stylize_markdown" = true;
+        };
+      };
     };
   };
 }
