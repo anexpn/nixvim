096027b9b6f056a93c425e85d90701d6d188dbc3
Jun <875241499@qq.com>
2025-12-09 03:04:51 +0800

    Set flash_telescope

--- config/plugins/ui/telescope.nix
+++ config/plugins/ui/telescope.nix
@@ -20,6 +20,18 @@
           };
         };
         sorting_strategy = "ascending";
+        mappings = {
+          n = {
+            "s" = {
+              __raw = "flash_telescope";
+            };
+          };
+          i = {
+            "<C-s>" = {
+              __raw = "flash_telescope";
+            };
+          };
+        };
       };
       pickers = {
         colorscheme = {
@@ -228,4 +240,25 @@
       };
     }
   ];
+
+  extraConfigLua = ''
+    function flash_telescope(prompt_bufnr)
+      require("flash").jump({
+        pattern = "^",
+        label = { after = { 0, 0 } },
+        search = {
+          mode = "search",
+          exclude = {
+            function(win)
+              return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults"
+            end,
+          },
+        },
+        action = function(match)
+          local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+          picker:set_selection(match.pos[1] - 1)
+        end,
+      })
+    end
+  '';
 }
